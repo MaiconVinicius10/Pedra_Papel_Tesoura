2e7d5ece9ec0ede6f0086d678f0b30dd9277c214
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-12-02 15:51:47 -0300

    Funcionalidade de selecionar aleatorio funcionando

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,17 +17,48 @@ public class MainActivity extends AppCompatActivity {
     }
     // metodo selecionar Pedra
     public void selecionarPedra(View view){
-        verificarGanhador("Pedra");
+        verificarGanhador("pedra");
     }
     //Metado Selecionar Papel
     public void selecionarPapel(View view){
-        verificarGanhador("Papel");
+        verificarGanhador("papel");
     }
     //metodo selecionarTesoura
     public void selecionarTesoura(View view){
-        verificarGanhador("Tesoura");
+        verificarGanhador("tesoura");
     }
+
+    //criando meu gerador de escolha da maquina
+    private String gerarEscolhaAleatoriaApp(){
+        //criando array aleatorio
+        String[]opcoes={"pedra,papel,tesoura"};
+        int numeroAleatorio=  new Random().nextInt(3);
+
+        //intanciando imagem
+        ImageView imagemApp=findViewById(R.id.image_app);
+
+        //numeroaleatorio
+        String escolhaApp=opcoes[numeroAleatorio];
+        switch (escolhaApp){
+            case "pedra":
+                imagemApp.setImageResource(R.drawable.pedra);
+                break;
+
+            case "papel":
+                imagemApp.setImageResource(R.drawable.papel);
+                break;
+
+            case "tesoura":
+                imagemApp.setImageResource(R.drawable.tesoura);
+        }
+
+        return escolhaApp;
+    }
+
     private void verificarGanhador(String escolhaUsuario){
-        System.out.println("Item clicado:"+ escolhaUsuario);
+
+        String escolha = gerarEscolhaAleatoriaApp();
+
+        // System.out.println("Item clicado:"+ escolhaUsuario);
     }
 }
