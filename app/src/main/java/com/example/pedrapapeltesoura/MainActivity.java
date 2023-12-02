4fe1fee1ddabbc02f9ee83def37dd7ff4d517fed
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-12-01 21:33:39 -0300

    Criando metodos para pedra papel e tesoura, testes ok no logcat

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // metodo selecionar Pedra
+    public void selecionarPedra(View view){
+        verificarGanhador("Pedra");
+    }
+    //Metado Selecionar Papel
+    public void selecionarPapel(View view){
+        verificarGanhador("Papel");
+    }
+    //metodo selecionarTesoura
+    public void selecionarTesoura(View view){
+        verificarGanhador("Tesoura");
+    }
+    private void verificarGanhador(String escolhaUsuario){
+        System.out.println("Item clicado:"+ escolhaUsuario);
     }
 }
