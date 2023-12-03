61a6c50d6bd5049e52874ef871e2949e3166d36f
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-12-03 00:07:36 -0300

    Funcionalidade de selecionar aleatorio funcionando

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Random;
 
@@ -15,50 +18,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     // metodo selecionar Pedra
-    public void selecionarPedra(View view){
+    public void selecionarPedra(View view) {
         verificarGanhador("pedra");
     }
+
     //Metado Selecionar Papel
-    public void selecionarPapel(View view){
+    public void selecionarPapel(View view) {
         verificarGanhador("papel");
     }
+
     //metodo selecionarTesoura
-    public void selecionarTesoura(View view){
+    public void selecionarTesoura(View view) {
         verificarGanhador("tesoura");
     }
 
-    //criando meu gerador de escolha da maquina
-    private String gerarEscolhaAleatoriaApp(){
-        //criando array aleatorio
-        String[]opcoes={"pedra,papel,tesoura"};
-        int numeroAleatorio=  new Random().nextInt(3);
+    //criando metodo gerarEscolha
+    private String gerarEscolhaAleatoriaApp() {
 
-        //intanciando imagem
-        ImageView imagemApp=findViewById(R.id.image_app);
+        //criar um array
+        String[] opcoes = {"pedra", "papel", "tesoura"};
 
-        //numeroaleatorio
-        String escolhaApp=opcoes[numeroAleatorio];
-        switch (escolhaApp){
+        //random
+        int numeroAleatorio = new Random().nextInt(3);
+
+        //recebendo imagem view
+        ImageView imageApp = findViewById(R.id.image_app);
+
+        String escolhaApp = opcoes[numeroAleatorio];
+        //criando  meu switch
+        switch (escolhaApp) {
             case "pedra":
-                imagemApp.setImageResource(R.drawable.pedra);
+                imageApp.setImageResource(R.drawable.pedra);
                 break;
-
             case "papel":
-                imagemApp.setImageResource(R.drawable.papel);
+                imageApp.setImageResource(R.drawable.papel);
                 break;
-
             case "tesoura":
-                imagemApp.setImageResource(R.drawable.tesoura);
+                imageApp.setImageResource(R.drawable.tesoura);
+                break;
         }
-
         return escolhaApp;
     }
 
-    private void verificarGanhador(String escolhaUsuario){
 
-        String escolha = gerarEscolhaAleatoriaApp();
+    private void verificarGanhador(String escolhaUsuario) {
 
+
+        String escolhaApp = gerarEscolhaAleatoriaApp();
+
+        TextView textResultado = findViewById(R.id.text_Resultado);
+
+        if ((escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+                (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+                (escolhaApp == "papel" && escolhaUsuario == "pedra")) {
+            textResultado.setText("Você perdeu :(");
+        } else if ((escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+                (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+                (escolhaUsuario == "papel" && escolhaApp == "pedra")) {
+            textResultado.setText("Você ganho :)");
+        } else {
+            textResultado.setText("Empatou");
+        }
         // System.out.println("Item clicado:"+ escolhaUsuario);
     }
+
 }
